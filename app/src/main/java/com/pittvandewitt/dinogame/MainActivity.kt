080e90b9500ccaf7c96b40d45d7f60df592bd9d9
pittvandewitt <thomasdewitt@outlook.com>
2019-11-01 15:55:00 +0100

    Pause/resume webview to save battery

--- app/src/main/java/com/pittvandewitt/dinogame/MainActivity.kt
+++ app/src/main/java/com/pittvandewitt/dinogame/MainActivity.kt
@@ -3,40 +3,33 @@ package com.pittvandewitt.dinogame
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
-import android.webkit.WebViewClient
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    private var mWebView: WebView? = null
+
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val mWebView = WebView(this)
-
-        mWebView.clearCache(true)
-        mWebView.clearHistory()
-        mWebView.settings.apply {
-            javaScriptEnabled = true
-            domStorageEnabled = true
-        }
-
-        val activity = this
-
-        mWebView.webViewClient = object : WebViewClient() {
-            override fun onReceivedError(
-                view: WebView,
-                errorCode: Int,
-                description: String,
-                failingUrl: String
-            ) {
-                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show()
+        mWebView = WebView(this)
+            .apply {
+                settings.apply {
+                    javaScriptEnabled = true
+                    domStorageEnabled = true
+                }
+                loadUrl("https://dinogame-client.netlify.com/")
             }
-        }
-
-        mWebView.loadUrl("https://dinogame-client.netlify.com/")
         setContentView(mWebView)
     }
-}
 
+    override fun onPause() {
+        super.onPause()
+        mWebView?.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mWebView?.onResume()
+    }
+}
